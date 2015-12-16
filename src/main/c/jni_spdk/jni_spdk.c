667b80982fd67072d8f0de3214805e0ac7758aad
anzhongqi <anzhongqi@gmail.com>
2015-12-16 21:50:40 +0800

    jni_spdk.c: update for java class/package.

--- src/main/c/jni_spdk/jni_spdk.c
+++ src/main/c/jni_spdk/jni_spdk.c
@@ -14,9 +14,11 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
 		return JNI_ERR;
 	}
 
-//	if (env->RegisterNatives(env->FindClass("Lac/ncic/syssw/jni/ubmk/JniUbmk;"),
+	printf("hello world from JNI_OnLoad!\n");
+
+//	if (env->RegisterNatives(env->FindClass("Lac/ncic/syssw/jni/spdk/JniSpdk;"),
 	if ((*env)->RegisterNatives(env,
-	                            (*env)->FindClass(env, "Lac/ncic/syssw/jni/spdk/Identify;"),
+	                            (*env)->FindClass(env, "Lac/ncic/syssw/jni/spdk/JniSpdk;"),
 	                            methods,
 	                            sizeof(methods) / sizeof(methods[0])
 	                           ) < -1) {
---
+++ src/main/java/ac/ncic/syssw/jni/spdk/Main.java
@@ -0,0 +1,7 @@
+package ac.ncic.syssw.jni.spdk;
+
+/**
+ * Created by azq on 15-12-16.
+ */
+public class Main {
+}
