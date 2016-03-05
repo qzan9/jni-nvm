ee050f5a48e27ee364016ad148dee5a6485eb6d9
anzhongqi <anzhongqi@gmail.com>
2016-03-05 22:12:23 +0800

    nvme_naive: benchmark latency of one single request [cmd->poll].

--- src/main/c/nvme_naive/nvme_naive.c
+++ src/main/c/nvme_naive/nvme_naive.c
@@ -16,19 +16,28 @@
 
 #include <spdk/nvme.h>
 
-#define U2_REQUEST_POOL_SIZE		(8192)
-#define U2_REQUEST_CACHE_SIZE		(128)
+#define U2_REQUEST_POOL_SIZE		(65536)
+#define U2_REQUEST_CACHE_SIZE		(0)
 #define U2_REQUEST_PRIVATE_SIZE		(0)
 
 #define U2_NAMESPACE_ID			(1)
-#define U2_QUEUE_DEPTH			(7168)
+#define U2_QUEUE_DEPTH			(512)
 
 #define U2_SIZE_512B			(512)
+#define U2_SIZE_1KB			(1024)
 #define U2_SIZE_4KB			(4096)
+#define U2_SIZE_16KB			(16384)
+#define U2_SIZE_64KB			(65536)
 #define U2_SIZE_128KB			(131072)
+#define U2_SIZE_256KB			(262144)
+#define U2_SIZE_512KB			(524288)
 #define U2_SIZE_1MB			(1048576)
+#define U2_SIZE_2MB			(2097152)
+#define U2_SIZE_4MB			(4194304)
 
-#define U2_SLAB_SIZE			(1048576)
+#define U2_SIZE_MAX			(U2_SIZE_4MB)
+
+#define U2_SLAB_SIZE			(U2_SIZE_1MB)
 
 #define U2_BUFFER_ALIGN			(0x200)
 
@@ -43,62 +52,105 @@ static uint64_t u2_ns_size;
 static char *ealargs[] = { "nvme_simple", "-c 0x1", "-n 4", };
 
 static int
-lat_bmk(uint32_t payload_size)
+u2_lat_bmk(int payload_size, int bmk_iter)
 {
-	uint32_t io_size, io_size_blocks;
+	uint32_t io_size, queue_depth;
+
+	uint32_t io_size_blocks;
 	uint64_t offset_in_ios, size_in_ios;
-	void *rd_buf, *wr_buf;
+	void *wr_buf, *rd_buf;
+	//void **wr_buf, **rd_buf;
 
 	uint64_t tsc_rate, tsc_start;
-	int rd_lat, wr_lat;
+	uint64_t wr_lat, rd_lat;
 
-	int i;
+	int i, rc;
 
 	io_size = payload_size;
+	queue_depth = bmk_iter;
+
 	io_size_blocks = io_size / u2_ns_sector;
 	offset_in_ios = 0;
 	size_in_ios = u2_ns_size / io_size;
 
-	rd_lat = 0;
-	wr_lat = 0;
+	//wr_buf = malloc(queue_depth * sizeof(void *));
+	//if (wr_buf == NULL) {
+	//	fprintf(stderr, "failed to malloc write buffer array!\n");
+	//	return 1;
+	//}
+	//rd_buf = malloc(queue_depth * sizeof(void *));
+	//if (rd_buf == NULL) {
+	//	fprintf(stderr, "failed to malloc read buffer array!\n");
+	//	return 1;
+	//}
+	//for (i = 0; i < queue_depth; i++) {
+	//	wr_buf[i] = rte_malloc(NULL, io_size, U2_BUFFER_ALIGN);
+	//	if (wr_buf[i] == NULL) {
+	//		fprintf(stderr, "failed to rte_malloc write buffer element!\n");
+	//		return 1;
+	//	}
+	//	rd_buf[i] = rte_malloc(NULL, io_size, U2_BUFFER_ALIGN);
+	//	if (rd_buf[i] == NULL) {
+	//		fprintf(stderr, "failed to rte_malloc read buffer element!\n");
+	//		return 1;
+	//	}
+	//}
 
 	tsc_rate = rte_get_tsc_hz();
+	wr_lat = 0;
+	rd_lat = 0;
 
-	for (i = 0; i < U2_QUEUE_DEPTH; i++) {
-		rd_buf = rte_malloc(NULL, io_size, U2_BUFFER_ALIGN);
+	if (spdk_nvme_register_io_thread()) {
+		fprintf(stderr, "failed to register thread!\n");
+		return 1;
+	}
+
+	for (i = 0; i < queue_depth; i++) {
 		wr_buf = rte_malloc(NULL, io_size, U2_BUFFER_ALIGN);
-
-		if (offset_in_ios == size_in_ios) {
-			offset_in_ios = 0;
+		if (wr_buf == NULL) {
+			fprintf(stderr, "failed to rte_malloc write buffer %d!\n", i);
+			return 1;
 		}
-
 		tsc_start = rte_get_timer_cycles();
-		if (spdk_nvme_ns_cmd_write(u2_ns, wr_buf, i * io_size_blocks, io_size_blocks, NULL, NULL, 0)) {
-			fprintf(stderr, "failed to submit write request %d!\n", i);
+		if (rc = spdk_nvme_ns_cmd_write(u2_ns, wr_buf, offset_in_ios * io_size_blocks, io_size_blocks, NULL, NULL, 0)) {
+			fprintf(stderr, "failed to submit write request %d, error %d!\n", i, rc);
 			return 1;
 		}
 		while (spdk_nvme_ctrlr_process_io_completions(u2_ctrlr, 0) != 1);
 		wr_lat += rte_get_timer_cycles() - tsc_start;
+		rte_free(wr_buf);
 
+		rd_buf = rte_malloc(NULL, io_size, U2_BUFFER_ALIGN);
+		if (rd_buf == NULL) {
+			fprintf(stderr, "failed to rte_malloc read buffer %d!\n", i);
+			return 1;
+		}
 		tsc_start = rte_get_timer_cycles();
-		if (spdk_nvme_ns_cmd_read(u2_ns, rd_buf, i * io_size_blocks, io_size_blocks, NULL, NULL, 0)) {
-			fprintf(stderr, "failed to submit read request %d!\n", i);
+		if (rc = spdk_nvme_ns_cmd_read(u2_ns, rd_buf, offset_in_ios * io_size_blocks, io_size_blocks, NULL, NULL, 0)) {
+			fprintf(stderr, "failed to submit read request %d, error %d!\n", i, rc);
 			return 1;
 		}
 		while (spdk_nvme_ctrlr_process_io_completions(u2_ctrlr, 0) != 1);
 		rd_lat += rte_get_timer_cycles() - tsc_start;
 
-		if (!memcmp(rd_buf, wr_buf, io_size)) {
-			fprintf(stderr, "oops, error R/W SSD ...\n");
-			return 1;
-		}
-
 		rte_free(rd_buf);
-		rte_free(wr_buf);
+
+		if (++offset_in_ios == size_in_ios) {
+			offset_in_ios = 0;
+		}
 	}
 
-	printf("read latency: %d us\n", rd_lat * 1000 * 1000 / U2_QUEUE_DEPTH / tsc_rate);
-	printf("write latency: %d us\n", wr_lat * 1000 * 1000 / U2_QUEUE_DEPTH / tsc_rate);
+	spdk_nvme_unregister_io_thread();
+
+	//for (i = 0; i < queue_depth; i++) {
+	//	rte_free(wr_buf[i]);
+	//	rte_free(rd_buf[i]);
+	//}
+	//free(wr_buf);
+	//free(rd_buf);
+
+	printf("write latency: %5.1f us\n", (float) (wr_lat * 1000000) / (queue_depth * tsc_rate));
+	printf("read latency:  %5.1f us\n", (float) (rd_lat * 1000000) / (queue_depth * tsc_rate));
 
 	return 0;
 }
@@ -111,7 +163,7 @@ probe_cb(void *cb_ctx, struct spdk_pci_device *dev)
 	}
 
 	if (spdk_pci_device_has_non_uio_driver(dev)) {
-		fprintf(stderr, "non-uio/kernel driver detected!\n");
+		fprintf(stderr, "non-UIO/kernel driver detected!\n");
 		return false;
 	}
 
@@ -131,7 +183,22 @@ attach_cb(void *cb_ctx, struct spdk_pci_device *dev, struct spdk_nvme_ctrlr *ctr
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int io_size_bytes, bmk_iter;
+	int op;
+
+	io_size_bytes = U2_SIZE_512B;
+	bmk_iter = U2_QUEUE_DEPTH;
+
+	while ((op = getopt(argc, argv, "q:")) != -1) {
+		switch (op) {
+		case 'q':
+			bmk_iter = atoi(optarg);
+			break;
+		default:
+			printf("%s -q [queue depth]\n", argv[0]);
+			return 1;
+		}
+	}
 
 	if (rte_eal_init(sizeof(ealargs) / sizeof(ealargs[0]), ealargs) < 0) {
 		fprintf(stderr, "failed to initialize DPDK EAL!\n");
@@ -153,24 +220,17 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if (lat_bmk(U2_SIZE_512B)) {
-		fprintf(stderr, "failed to benchmark 512B I/O latency!\n");
-		return 1;
-	}
+	while (1) {
+		printf("latency benchmarking - IO size %d ...\n", io_size_bytes);
 
-	if (lat_bmk(U2_SIZE_4KB)) {
-		fprintf(stderr, "failed to benchmark 4KB I/O latency!\n");
-		return 1;
-	}
+		if (u2_lat_bmk(io_size_bytes, bmk_iter)) {
+			fprintf(stderr, "failed to benchmark latency - IO size %d!\n", io_size_bytes);
+			return 1;
+		}
 
-	if (lat_bmk(U2_SIZE_128KB)) {
-		fprintf(stderr, "failed to benchmark 128KB I/O latency!\n");
-		return 1;
-	}
-
-	if (lat_bmk(U2_SIZE_1MB)) {
-		fprintf(stderr, "failed to benchmark 1MB I/O latency!\n");
-		return 1;
+		if ((io_size_bytes *= 2) > U2_SIZE_MAX) {
+			break;
+		}
 	}
 
 	spdk_nvme_detach(u2_ctrlr);
