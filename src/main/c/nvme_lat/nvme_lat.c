0f806ac618630defceaa438d46f3bb0d964d5821
anzhongqi <anzhongqi@gmail.com>
2016-05-04 09:16:55 +0800

    nvme_lat: output re-formatted.

--- src/main/c/nvme_lat/nvme_lat.c
+++ src/main/c/nvme_lat/nvme_lat.c
@@ -1,7 +1,7 @@
 /*
  * nvme_lat/u2_lat: simple latency benchmarking.
  *
- *   RESTRICTED to just ONE thread and ONE controller and ONE namespace.
+ * NOTE: RESTRICTED to just ONE thread and ONE controller and ONE namespace.
  */
 
 #include <stdio.h>
@@ -19,39 +19,37 @@
 
 #include <spdk/nvme.h>
 
-#define U2_REQUEST_POOL_SIZE		(65536)
-#define U2_REQUEST_CACHE_SIZE		(0)
-#define U2_REQUEST_PRIVATE_SIZE		(0)
+#define U2_REQUEST_POOL_SIZE    (1024)
+#define U2_REQUEST_CACHE_SIZE   (0)
+#define U2_REQUEST_PRIVATE_SIZE (0)
 
-#define U2_NAMESPACE_ID			(1)
+#define U2_NAMESPACE_ID         (1)
 
-#define U2_IO_NUM			(8192)
+#define U2_SIZE_512B            (512)
+#define U2_SIZE_1KB             (1024)
+#define U2_SIZE_4KB             (4096)
+#define U2_SIZE_8KB             (8192)
+#define U2_SIZE_16KB            (16384)
+#define U2_SIZE_64KB            (65536)
+#define U2_SIZE_128KB           (131072)
+#define U2_SIZE_256KB           (262144)
+#define U2_SIZE_512KB           (524288)
+#define U2_SIZE_1MB             (1048576)
+#define U2_SIZE_2MB             (2097152)
+#define U2_SIZE_4MB             (4194304)
 
-#define U2_SIZE_512B			(512)
-#define U2_SIZE_1KB			(1024)
-#define U2_SIZE_4KB			(4096)
-#define U2_SIZE_8KB			(8192)
-#define U2_SIZE_16KB			(16384)
-#define U2_SIZE_64KB			(65536)
-#define U2_SIZE_128KB			(131072)
-#define U2_SIZE_256KB			(262144)
-#define U2_SIZE_512KB			(524288)
-#define U2_SIZE_1MB			(1048576)
-#define U2_SIZE_2MB			(2097152)
-#define U2_SIZE_4MB			(4194304)
+#define U2_IO_SIZE_MIN          (U2_SIZE_512B)
+#define U2_IO_SIZE_MAX          (U2_SIZE_4MB)
+#define U2_BUFFER_ALIGN         (0x200)
 
-#define U2_SIZE_MIN			(U2_SIZE_4KB)
-#define U2_SIZE_MAX			(U2_SIZE_4MB)
+#define U2_IO_NUM               (8192)
 
-#define U2_SLAB_SIZE			(U2_SIZE_1MB)
+#define U2_RANDOM               (1)
+#define U2_SEQUENTIAL           (0)
+#define U2_READ                 (1)
+#define U2_WRITE                (0)
 
-#define U2_BUFFER_ALIGN			(0x200)
-
-#define U2_RANDOM			(1)
-#define U2_SEQUENTIAL			(0)
-
-#define U2_READ				(1)
-#define U2_WRITE			(0)
+//#define U2_TIME_SEC             (4)
 
 static struct spdk_nvme_ctrlr *u2_ctrlr;
 
@@ -63,7 +61,7 @@ static uint64_t u2_ns_size;
 static struct spdk_nvme_qpair *u2_qpair;
 
 static uint32_t io_size;
-static uint32_t io_num;
+static uint64_t io_num;
 static uint32_t io_depth;
 
 static uint8_t is_random;
@@ -71,6 +69,7 @@ static uint8_t is_rw;
 
 static char *core_mask;
 static uint8_t mem_chn;
+//static uint32_t time_in_sec;
 
 struct rte_mempool *request_mempool;
 static char *ealargs[] = { "nvme_lat", "-c 0x1", "-n 1", };
@@ -87,9 +86,10 @@ parse_args(int argc, char **argv)
 	u2_ns_id = U2_NAMESPACE_ID;
 	u2_ns = NULL;
 
-	io_size = U2_SIZE_MIN;
+	io_size = U2_IO_SIZE_MIN;
 
 	while ((op = getopt(argc, argv, "q:w:c:n:")) != -1) {
+	//while ((op = getopt(argc, argv, "w:c:n:t:")) != -1) {
 		switch (op) {
 		case 'q':
 			io_num = atoi(optarg);
@@ -103,12 +103,15 @@ parse_args(int argc, char **argv)
 		case 'n':
 			mem_chn = atoi(optarg);
 			break;
+		//case 't':
+		//	time_in_sec = atoi(optarg);
+		//	break;
 		default:
 			return 1;
 		}
 	}
 
-	if (!io_num || io_num >= U2_REQUEST_POOL_SIZE) {
+	if (!io_num) {
 		io_num = U2_IO_NUM;
 	}
 
@@ -152,6 +155,10 @@ parse_args(int argc, char **argv)
 		mem_chn = 1;
 	}
 
+	//if (!time_in_sec) {
+	//	time_in_sec = U2_TIME_SEC;
+	//}
+
 	return 0;
 }
 
@@ -164,10 +171,10 @@ probe_cb(void *cb_ctx, struct spdk_pci_device *dev, struct spdk_nvme_ctrlr_opts 
 
 	if (spdk_pci_device_has_non_uio_driver(dev)) {
 		fprintf(stderr, "%04x:%02x:%02x.%02x: non-UIO/kernel driver detected!\n",
-				spdk_pci_device_get_domain(dev),
-				spdk_pci_device_get_bus(dev),
-				spdk_pci_device_get_dev(dev),
-				spdk_pci_device_get_func(dev));
+		                spdk_pci_device_get_domain(dev),
+		                spdk_pci_device_get_bus(dev),
+		                spdk_pci_device_get_dev(dev),
+		                spdk_pci_device_get_func(dev));
 		return false;
 	}
 
@@ -184,10 +191,10 @@ attach_cb(void *cb_ctx, struct spdk_pci_device *dev, struct spdk_nvme_ctrlr *ctr
 	u2_qpair = spdk_nvme_ctrlr_alloc_io_qpair(u2_ctrlr, 0);
 
 	printf("attached to %04x:%02x:%02x.%02x!\n",
-			spdk_pci_device_get_domain(dev),
-			spdk_pci_device_get_bus(dev),
-			spdk_pci_device_get_dev(dev),
-			spdk_pci_device_get_func(dev));
+	       spdk_pci_device_get_domain(dev),
+	       spdk_pci_device_get_bus(dev),
+	       spdk_pci_device_get_dev(dev),
+	       spdk_pci_device_get_func(dev));
 }
 
 static int
@@ -198,15 +205,15 @@ u2_init(void)
 		return 1;
 	}
 
-	printf("\n====================================\n");
-	printf(  "  NVMe/U2 LAT - ict.ncic.syssw.ufo"    );
-	printf("\n====================================\n");
+	printf("\n========================================\n");
+	printf(  "  nvme_lat/u2_lat - ict.ncic.syssw.ufo"    );
+	printf("\n========================================\n");
 
 	request_mempool = rte_mempool_create("nvme_request",
-				U2_REQUEST_POOL_SIZE, spdk_nvme_request_size(),
-				U2_REQUEST_CACHE_SIZE, U2_REQUEST_PRIVATE_SIZE,
-				NULL, NULL, NULL, NULL,
-				SOCKET_ID_ANY, 0);
+	                                     U2_REQUEST_POOL_SIZE, spdk_nvme_request_size(),
+	                                     U2_REQUEST_CACHE_SIZE, U2_REQUEST_PRIVATE_SIZE,
+	                                     NULL, NULL, NULL, NULL,
+	                                     SOCKET_ID_ANY, 0);
 	if (request_mempool == NULL) {
 		fprintf(stderr, "failed to create request pool!\n");
 		return 1;
@@ -227,7 +234,7 @@ u2_init(void)
 		return 1;
 	}
 
-	//if (u2_ns_size < io_size || u2_ns_size > io_size) {
+	//if (u2_ns_size < (uint64_t) io_size || u2_ns_size > (uint64_t) io_size) {
 	//	fprintf(stderr, "invalid namespace size %"PRIu64"!\n", u2_ns_size);
 	//	return 1;
 	//}
@@ -243,6 +250,7 @@ u2_init(void)
 static void
 u2_io_complete(void *cb_args, const struct spdk_nvme_cpl *completion)
 {
+	//io_num++;
 	io_depth--;
 }
 
@@ -250,21 +258,26 @@ static int
 u2_lat_bench(void)
 {
 	int i, rc;
+	//int rc;
 
 	void *buf;
+
 	uint32_t io_size_blocks;
 	uint64_t offset_in_ios, size_in_ios;
 
-	uint64_t tsc_elapsed, tsc_start;
 	uint64_t tsc_rate;
+	uint64_t tsc_start, tsc_elapsed;
+	//uint64_t tsc_end;
 
 	buf = rte_malloc(NULL, io_size, U2_BUFFER_ALIGN);
 	if (buf == NULL) {
 		fprintf(stderr, "failed to rte_malloc buffer!\n");
 		return 1;
 	}
-
 	memset(buf, 0xff, io_size);
+
+	//io_num = 0;
+	//io_depth = 0;
 
 	io_size_blocks = io_size / u2_ns_sector;
 	offset_in_ios = -1;
@@ -272,9 +285,10 @@ u2_lat_bench(void)
 
 	tsc_rate = rte_get_tsc_hz();
 	tsc_elapsed = 0;
-
 	tsc_start = rte_get_timer_cycles();
+	//tsc_end = rte_get_timer_cycles() + time_in_sec * tsc_rate;
 	for (i = 0; i < io_num; i++) {
+	//while (1) {
 		if (is_random) {
 			offset_in_ios = rand_r(&seed) % size_in_ios;
 		} else {
@@ -290,17 +304,27 @@ u2_lat_bench(void)
 		}
 		if (rc) {
 			fprintf(stderr, "failed to submit request %d!\n", i);
+			//fprintf(stderr, "failed to submit request %d!\n", io_num);
 			return rc;
 		}
-		io_depth++;
+		io_depth++;    // for latency benchmarking, queue depth stays at 1.
 
 		while (io_depth > 0) {
 			spdk_nvme_qpair_process_completions(u2_qpair, 0);
 		}
+
+		//if (rte_get_timer_cycles() > tsc_end) {
+		//	break;
+		//}
 	}
 	tsc_elapsed = rte_get_timer_cycles() - tsc_start;
 
-	printf("\t\t%9.1f us\n", (float) (tsc_elapsed * 1000000) / (io_num * tsc_rate));
+	printf("\t\t%9.1f us", (float) (tsc_elapsed * 1000000) / (io_num * tsc_rate));
+	printf("\t\t%10.1f s", (float) tsc_elapsed / tsc_rate);
+	printf("\n");
+
+	//printf("\t\t%9.1f us", (float) (time_in_sec * 1000000) / io_num);
+	//printf("\t\t%12"PRIu64"\n", io_num);
 
 	rte_free(buf);
 
@@ -310,9 +334,13 @@ u2_lat_bench(void)
 static void
 u2_cleanup(void)
 {
-	spdk_nvme_ctrlr_free_io_qpair(u2_qpair);
+	if (u2_qpair) {
+		spdk_nvme_ctrlr_free_io_qpair(u2_qpair);
+	}
 
-	spdk_nvme_detach(u2_ctrlr);
+	if (u2_ctrlr) {
+		spdk_nvme_detach(u2_ctrlr);
+	}
 
 	if (core_mask) {
 		free(ealargs[1]);
@@ -328,35 +356,42 @@ int main(int argc, char *argv[])
 	if (parse_args(argc, argv)) {
 		printf("usage: %s [OPTION]...\n", argv[0]);
 		printf("\t-q [IO number]\n");
-		printf("\t-w [IO mode (read, randread, write, randwrite)]\n");
+		printf("\t-w [workload type (read, randread, write, randwrite)]\n");
 		printf("\t-c [core mask]\n");
 		printf("\t-n [memory channels]\n");
-		return 1;
+		//printf("\t-t [time in seconds]\n");
+		goto FAIL;
 	}
 
 	if (u2_init()) {
 		fprintf(stderr, "failed to initialize u2 benchmarking context!\n");
-		return 1;
+		goto FAIL;
 	}
 
-	printf("u2 latency benchmarking ... queue depth: %d, RW type: %s %s\n",
-		io_num, is_random ? "random" : "sequential", is_rw ? "read" : "write");
-	printf("\t%8s\t\t%12s\n", "I/O size", "latency");
+	printf("u2 latency benchmarking ... RW type: %s %s, IOs: %"PRIu64"\n",
+	       is_random ? "random" : "sequential", is_rw ? "read" : "write", io_num);
+	printf("\t%8s\t\t%12s\t\t%12s\n", "I/O size", "latency", "elapsed time");
+	//printf("u2 latency benchmarking ... RW type: %s %s, Time (s): %"PRIu32"\n",
+	//       is_random ? "random" : "sequential", is_rw ? "read" : "write", time_in_sec);
+	//printf("\t%8s\t\t%12s\t\t%12s\n", "I/O size", "latency", "I/O count");
 	while (1) {
 		printf("\t%8d", io_size);
 
 		if (u2_lat_bench()) {
 			fprintf(stderr, "failed to benchmark latency - IO size %d!\n", io_size);
-			return 1;
+			goto FAIL;
 		}
 
-		if ((io_size *= 2) > U2_SIZE_MAX) {
+		if ((io_size *= 2) > U2_IO_SIZE_MAX) {
 			break;
 		}
 	}
 
 	u2_cleanup();
-
 	return 0;
+
+FAIL:
+	u2_cleanup();
+	return 1;
 }
 
